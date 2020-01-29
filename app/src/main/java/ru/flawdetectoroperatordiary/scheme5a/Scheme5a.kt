09120bd7e0197f29ff6e39de5decd4710ac3774c
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2020-01-29 21:26:40 +0500

    add scheme5a fragment and MainActivity

---
+++ app/src/main/java/ru/flawdetectoroperatordiary/scheme5a/Scheme5a.kt
@@ -0,0 +1,25 @@
+package ru.flawdetectoroperatordiary.scheme5a
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.flawdetectoroperatordiary.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Scheme5a : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scheme5a, container, false)
+    }
+
+
+}
