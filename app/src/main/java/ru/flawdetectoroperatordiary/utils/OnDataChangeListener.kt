e24836548b241f6e71cc4cf592188a5d42e0b1d8
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2020-02-05 23:38:13 +0500

    DataChange listener can erase data on request

--- app/src/main/java/ru/flawdetectoroperatordiary/utils/OnDataChangeListener.kt
+++ app/src/main/java/ru/flawdetectoroperatordiary/utils/OnDataChangeListener.kt
@@ -8,4 +8,9 @@ interface OnDataChangeListener {
      * When the data changes, the listener receives it.
      */
     fun onChange(value: Double)
+
+    /**
+     * When the data is to be erased, this function is called.
+     */
+    fun onErase()
 }
