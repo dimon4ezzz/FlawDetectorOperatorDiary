f661cd64c1f41cdbf51e345cc4ff0586ca4732d2
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2020-02-06 22:27:37 +0500

    extended all typical Scheme functions to `DefaultFragment`

--- app/src/main/java/ru/flawdetectoroperatordiary/scheme5a/Scheme5a.kt
+++ app/src/main/java/ru/flawdetectoroperatordiary/scheme5a/Scheme5a.kt
@@ -1,31 +1,14 @@
 package ru.flawdetectoroperatordiary.scheme5a
 
-import android.content.Context
-import android.os.Bundle
-import android.view.KeyEvent
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import ru.flawdetectoroperatordiary.R
-import ru.flawdetectoroperatordiary.utils.CommonMath
-import ru.flawdetectoroperatordiary.utils.Field
-import ru.flawdetectoroperatordiary.utils.OnDataChangeListener
-import ru.flawdetectoroperatordiary.utils.Scheme
+import ru.flawdetectoroperatordiary.utils.*
 
 private const val FORMAT = "%.4f"
 private const val INT_FORMAT = "%.0f"
 
-class Scheme5a : Fragment() {
-    private lateinit var externalDiameter: EditText
-    private lateinit var radiationThickness: EditText
-    private lateinit var sensitivity: EditText
-    private lateinit var focalSpot: EditText
-
+class Scheme5a : DefaultFragment() {
     private lateinit var internalDiameter: TextView
     private lateinit var coefC: TextView
     private lateinit var coefM: TextView
@@ -35,24 +18,16 @@ class Scheme5a : Fragment() {
     private lateinit var scansAmount: TextView
     private lateinit var plotLength: TextView
 
-    private val math = CommonMath(Scheme.FIVE_A)
-
-    private var counter = 4
-    private var wasEmpty = true
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_scheme5a, container, false)
+    override fun setActivityTitle() {
         activity!!.setTitle(R.string.scheme5a_title)
+    }
 
+    override fun setMath() {
+        math = CommonMath(Scheme.FIVE_A)
+    }
+
+    override fun initCalculatedFields(view: View) {
         with(view) {
-            externalDiameter = findViewById(R.id.et_external_diameter)
-            radiationThickness = findViewById(R.id.et_radiation_thickness)
-            sensitivity = findViewById(R.id.et_sensitivity)
-            focalSpot = findViewById(R.id.et_focal_spot)
-
             internalDiameter = findViewById(R.id.tv_internal_diameter)
             coefC = findViewById(R.id.tv_coef_c)
             coefM = findViewById(R.id.tv_coef_m)
@@ -62,38 +37,9 @@ class Scheme5a : Fragment() {
             scansAmount = findViewById(R.id.tv_scans_amount)
             plotLength = findViewById(R.id.tv_plot_length)
         }
-
-        setListeners()
-
-        // Inflate the layout for this fragment
-        return view
     }
 
-    private fun setListeners() {
-        externalDiameter.setOnEditorActionListener(defaultOnEditorActionListener())
-        externalDiameter.onFocusChangeListener = defaultOnFocusChangeListener(
-            set = { math.setExternalDiameter(it) },
-            unset = { math.unsetExternalDiameter() }
-        )
-
-        radiationThickness.setOnEditorActionListener(defaultOnEditorActionListener())
-        radiationThickness.onFocusChangeListener = defaultOnFocusChangeListener(
-            set = { math.setRadiationThickness(it) },
-            unset = { math.unsetRadiationThickness() }
-        )
-
-        sensitivity.setOnEditorActionListener(defaultOnEditorActionListener())
-        sensitivity.onFocusChangeListener = defaultOnFocusChangeListener(
-            set = { math.setSensitivity(it) },
-            unset = { math.unsetSensitivity() }
-        )
-
-        focalSpot.setOnEditorActionListener(defaultOnEditorActionListener())
-        focalSpot.onFocusChangeListener = defaultOnFocusChangeListener(
-            set = { math.setFocalSpot(it) },
-            unset = { math.unsetFocalSpot() }
-        )
-
+    override fun setCalculatedFieldListeners() {
         math.setListener(Field.INTERNAL_DIAMETER, object : OnDataChangeListener {
             override fun onChange(value: Double) {
                 internalDiameter.text = FORMAT.format(value)
@@ -183,66 +129,4 @@ class Scheme5a : Fragment() {
             }
         })
     }
-
-    private fun defaultOnEditorActionListener() =
-        TextView.OnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_NEXT ||
-                actionId == EditorInfo.IME_ACTION_DONE ||
-                event?.keyCode == KeyEvent.KEYCODE_ENTER ||
-                event?.keyCode == KeyEvent.KEYCODE_NUMPAD_ENTER ||
-                event?.keyCode == KeyEvent.KEYCODE_DPAD_CENTER
-            ) {
-                requestFocus(v)
-
-                // attribution: https://stackoverflow.com/a/1109108/3686575
-                if (actionId == EditorInfo.IME_ACTION_DONE)
-                    view?.let {
-                        it.clearFocus()
-                        val imm =
-                            activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-                        imm?.hideSoftInputFromWindow(it.windowToken, 0)
-                    }
-                return@OnEditorActionListener true
-            }
-
-            false
-        }
-
-    private fun requestFocus(v: TextView) {
-        when {
-            externalDiameter.text.isEmpty() && v != externalDiameter ->
-                externalDiameter.requestFocus()
-            radiationThickness.text.isEmpty() && v != radiationThickness ->
-                radiationThickness.requestFocus()
-            sensitivity.text.isEmpty() && v != sensitivity ->
-                sensitivity.requestFocus()
-            focalSpot.text.isEmpty() && v != focalSpot ->
-                focalSpot.requestFocus()
-        }
-    }
-
-    private fun defaultOnFocusChangeListener(set: (Double) -> Unit, unset: () -> Unit) =
-        View.OnFocusChangeListener { v, hasFocus ->
-            require(v is EditText) { "default on-focus change listener only for EditText" }
-
-            if (v.text.isNotEmpty())
-                set(v.text.toString().toDouble())
-            else
-                unset()
-
-            if (!hasFocus) {
-                if (v.text.isEmpty() && !wasEmpty) {
-                    counter++
-                } else if (v.text.isNotEmpty() && wasEmpty && counter > 1) {
-                    counter--
-                }
-            } else {
-                if (counter == 1)
-                    v.imeOptions = EditorInfo.IME_ACTION_DONE
-                else
-                    v.imeOptions = EditorInfo.IME_ACTION_NEXT
-
-                wasEmpty = v.text.isEmpty()
-            }
-        }
 }
